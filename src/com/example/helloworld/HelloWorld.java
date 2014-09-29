f73c9f4136666e9b974c2c82a9e59e33c1cd2a09
Bartuz <filip@bartuzi.me>
2014-09-30 00:00:59 +0200

    Init hello world app

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by filip on 29.09.2014.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello PJWSTK!");
+    }
+}
